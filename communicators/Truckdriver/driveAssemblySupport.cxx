9fa0f981fa3fb93c53d62b15f30611f4f1c36c5e
Rádai Ronald <gtadj.roni@gmail.com>
2022-09-26 21:46:45 +0200

    Drive over dds

---
+++ communicators/Truckdriver/driveAssemblySupport.cxx
@@ -0,0 +1,126 @@
+
+/*
+WARNING: THIS FILE IS AUTO-GENERATED. DO NOT MODIFY.
+
+This file was generated from driveAssembly.idl
+using RTI Code Generator (rtiddsgen) version 3.1.1.
+The rtiddsgen tool is part of the RTI Connext DDS distribution.
+For more information, type 'rtiddsgen -help' at a command shell
+or consult the Code Generator User's Manual.
+*/
+
+#include "driveAssemblySupport.h"
+#include "driveAssemblyPlugin.h"
+
+#ifndef dds_c_log_impl_h              
+#include "dds_c/dds_c_log_impl.h"                                
+#endif        
+
+/* ========================================================================= */
+/**
+<<IMPLEMENTATION>>
+
+Defines:   TData,
+TDataWriter,
+TDataReader,
+TTypeSupport
+
+Configure and implement 'DriveAssembly' support classes.
+
+Note: Only the #defined classes get defined
+*/
+
+/* ----------------------------------------------------------------- */
+/* DDSDataWriter
+*/
+
+/**
+<<IMPLEMENTATION >>
+
+Defines:   TDataWriter, TData
+*/
+
+/* Requires */
+#define TTYPENAME   DriveAssemblyTYPENAME
+
+/* Defines */
+#define TDataWriter DriveAssemblyDataWriter
+#define TData       DriveAssembly
+
+#define ENABLE_TDATAWRITER_DATA_CONSTRUCTOR_METHODS
+#include "dds_cpp/generic/dds_cpp_data_TDataWriter.gen"
+#undef ENABLE_TDATAWRITER_DATA_CONSTRUCTOR_METHODS
+
+#undef TDataWriter
+#undef TData
+
+#undef TTYPENAME
+
+/* ----------------------------------------------------------------- */
+/* DDSDataReader
+*/
+
+/**
+<<IMPLEMENTATION >>
+
+Defines:   TDataReader, TDataSeq, TData
+*/
+
+/* Requires */
+#define TTYPENAME   DriveAssemblyTYPENAME
+
+/* Defines */
+#define TDataReader DriveAssemblyDataReader
+#define TDataSeq    DriveAssemblySeq
+#define TData       DriveAssembly
+
+#define ENABLE_TDATAREADER_DATA_CONSISTENCY_CHECK_METHOD
+#include "dds_cpp/generic/dds_cpp_data_TDataReader.gen"
+#undef ENABLE_TDATAREADER_DATA_CONSISTENCY_CHECK_METHOD
+
+#undef TDataReader
+#undef TDataSeq
+#undef TData
+
+#undef TTYPENAME
+
+/* ----------------------------------------------------------------- */
+/* TypeSupport
+
+<<IMPLEMENTATION >>
+
+Requires:  TTYPENAME,
+TPlugin_new
+TPlugin_delete
+Defines:   TTypeSupport, TData, TDataReader, TDataWriter
+*/
+
+/* Requires */
+#define TTYPENAME    DriveAssemblyTYPENAME
+#define TPlugin_new  DriveAssemblyPlugin_new
+#define TPlugin_delete  DriveAssemblyPlugin_delete
+
+/* Defines */
+#define TTypeSupport DriveAssemblyTypeSupport
+#define TData        DriveAssembly
+#define TDataReader  DriveAssemblyDataReader
+#define TDataWriter  DriveAssemblyDataWriter
+#define TGENERATE_SER_CODE
+#ifndef NDDS_STANDALONE_TYPE
+#define TGENERATE_TYPECODE
+#endif
+
+#include "dds_cpp/generic/dds_cpp_data_TTypeSupport.gen"
+
+#undef TTypeSupport
+#undef TData
+#undef TDataReader
+#undef TDataWriter
+#ifndef NDDS_STANDALONE_TYPE
+#undef TGENERATE_TYPECODE
+#endif
+#undef TGENERATE_SER_CODE
+#undef TTYPENAME
+#undef TPlugin_new
+#undef TPlugin_delete
+
